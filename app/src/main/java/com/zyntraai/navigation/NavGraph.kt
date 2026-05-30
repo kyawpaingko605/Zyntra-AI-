6b1a90bebdf636725b766cd0fcd9e6ca0ba0fab1
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-05-30 18:23:04 +0700

    Update NavGraph.kt

--- app/src/main/java/com/zyntraai/navigation/NavGraph.kt
+++ app/src/main/java/com/zyntraai/navigation/NavGraph.kt
@@ -3,7 +3,7 @@ package com.zyntraai.navigation
 import androidx.compose.runtime.Composable
 import com.zyntraai.ui.voice.VoiceScreen
 import androidx.navigation.compose.*
-
+import com.zyntraai.ui.chat.ChatScreen
 import com.zyntraai.ui.home.HomeScreen
 import com.zyntraai.ui.onboarding.OnboardingScreen
 import com.zyntraai.ui.splash.SplashScreen
