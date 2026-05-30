22ced61a1814bc36e1c158e4f021d2e3f4920b0e
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-05-30 17:57:12 +0700

    Create NavGraph.kt

---
+++ app/src/main/java/com/zyntraai/navigation/NavGraph.kt
@@ -0,0 +1,44 @@
+package com.zyntraai.navigation
+
+import androidx.compose.runtime.Composable
+
+import androidx.navigation.compose.*
+
+import com.zyntraai.ui.home.HomeScreen
+import com.zyntraai.ui.onboarding.OnboardingScreen
+import com.zyntraai.ui.splash.SplashScreen
+
+@Composable
+fun NavGraph() {
+
+    val navController =
+        rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination =
+        Screen.Splash.route
+    ) {
+
+        composable(
+            Screen.Splash.route
+        ) {
+
+            SplashScreen(navController)
+        }
+
+        composable(
+            Screen.Onboarding.route
+        ) {
+
+            OnboardingScreen(navController)
+        }
+
+        composable(
+            Screen.Home.route
+        ) {
+
+            HomeScreen()
+        }
+    }
+}
