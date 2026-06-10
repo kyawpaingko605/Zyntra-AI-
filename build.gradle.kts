b22b74634117c7c0f1d8a5730a44ba76960919b8
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-06-10 21:19:26 +0700

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    // Android Application plugin နှင့် ဗားရှင်း သတ်မှတ်ခြင်း
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
